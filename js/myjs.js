12cef321a25f483441b71bd2c7eb1406965851b4
Neftalí Acosta <hola@neftaliacosta.com>
2016-07-29 21:49:33 -0500

    V 0.51
    + Efecto en el menu
    + Fixed en soluciones
    +Barra de progreso

--- js/myjs.js
+++ js/myjs.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
-	$('.mysos img').click(function(){
+	$("#item1").hide();
+	$("#item2").hide();
+	$("#item3").hide();
+	$("#item4").hide();
+	$("#item5").hide();
+	$("#item1").show("fast",function(){
+		$("#item2").show("fast",function(){
+			$("#item3").show("fast",function(){	
+				$("#item4").show("fast",function(){	
+					$("#item5").show("fast",function(){		
+					});	
+				});	
+			});
+		});
+	});
+
+	/*Detalles de Servicios*/
+	$('.mysos img').click(function(env){
 		myid = $(this).attr("id");
-		$('.mysos').hide();
+		$('.mysos').fadeOut(1000);
 		$('#detalles').html("<img src='img/"+myid+".png' class='img-responsive'>");
+		$('#detalles').fadeIn(1000);
 	});
 
 	$('#detalles').click(function(){
-		$('#detalles').hide();
-		$('.mysos').show();
+		$('#detalles').fadeOut(1000);
+		$('.mysos').fadeIn(1000);
 		$('#detalles').html("");
 	});
 });
