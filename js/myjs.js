91b9671d55ca8fee1aaf6c74d0b0e1bf939e262c
Neftalí Acosta <hola@neftaliacosta.com>
2016-08-01 16:17:29 -0500

    V 0.56
    Fix:
    -Fuentes
    -Menu
    -Soluciones
    -Ubicaciones
    -Footer
    -Formulario de contacto

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	var dirm = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.9668179883383!2d-100.30688428497957!3d25.738610883647308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662945898d24689%3A0x791182c8d770a209!2sLope+de+Vega+200%2C+An%C3%A1huac%2C+66450+San+Nicol%C3%A1s+de+los+Garza%2C+N.L.!5e0!3m2!1ses-419!2smx!4v1470079143034";
+	var dirg = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.5621307995075!2d-103.4340892163556!3d20.68738585648423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428aec167b0cd73%3A0xf1594ecd65f02578!2sAv.+Inglaterra+7030%2C+Paseos+Universidad%2C+45016+Zapopan%2C+Jal.!5e0!3m2!1ses-419!2smx!4v1469819044573";
 	$("#item1").hide();
 	$("#item2").hide();
 	$("#item3").hide();
@@ -19,6 +21,20 @@ $(document).ready(function(){
 		});
 	});
 
+	/*Inicio mapa*/
+
+	$('#guad').click(function(){
+		$('#mymapa iframe').attr("src",dirg);
+		$('#guad').css('color','#383a43');
+		$('#mty').css('color','#0070bb');
+	})
+	$('#mty').click(function(){
+		$('#mymapa iframe').attr("src",dirm);
+		$('#guad').css('color','#0070bb');
+		$('#mty').css('color','#383a43');
+	})
+	/*Fin mapa*/
+
 	/*Detalles de Servicios*/
 	$('.mysos img').click(function(env){
 		myid = $(this).attr("id");
@@ -28,9 +44,11 @@ $(document).ready(function(){
 	});
 
 	$('#detalles').click(function(){
+		var pathname = window.location.pathname;
 		$('#detalles').fadeOut(1000);
 		$('.mysos').fadeIn(1000);
 		$('#detalles').html("");
+		window.location.href = pathname+"#soluciones";
 	});
 
 	/*Facebook*/
@@ -68,44 +86,45 @@ $(document).ready(function(){
 		var Estado = $('#estado').val();
 		var Asunto = $('#asunto').val();
 		var Mensaje =$('#mensaje').val();
+		if (Empresa==""){
+				Reserva="N/A"; 
+			}
 		
 		
 			if (Nombre=="") {
 				$('#msj').html("Por favor escriba su Nombre Completo.");
 				$('#nombre').focus();
-				$('#notificacion').modal('show');
+				
 			}
 			else if (Correo=="") {
 				$('#msj').html(Nombre + " por favor comp\u00e1rtanos su E-mail.");
 				$('#correo').focus();
-				$('#notificacion').modal('show');
+				
 			}
 			else if (Telefono=="") {
 				$('#msj').html(Nombre + " por favor digite su n\u00famero de teléfono.");
 				$('#telefono').focus();
-				$('#notificacion').modal('show');
+				
 			}
 			else if (Estado=="") {
 				$('#msj').html(Nombre + " por favor díganos de que parte nos escribe.");
 				$('#estado').focus();
-				$('#notificacion').modal('show');
+				
 			}
 			else if (Asunto=="*Requiero") {
 				$('#msj').html(Nombre + " por favor seleccione un asunto.");
 				$('#asunto').focus();
-				$('#notificacion').modal('show');
+				
 			}
 			else if (Mensaje==""){
 				$('#msj').html(Nombre + " por favor escriba su mensaje");
 				$('#mensaje').focus();
-				$('#notificacion').modal('show');
+				
 			}
 			
 			else{
 				
-				if (Empresa==""){
-				Reserva="N/A"; 
-				}
+				
 				var urlData ="&url=" + url +
 							"&Nombre=" + Nombre +
 							"&Empresa=" + Empresa +
@@ -130,13 +149,7 @@ $(document).ready(function(){
 	        });
 			
 			}
-		('#nombre').val('');
-		('#empresa').val('');
-		('#correo').val('');
-		('#telefono').val('');
-		('#estado').val('');
-		('#asunto').val('');
-		('#mensaje').val();
+
 		
 		return false;
 		});
